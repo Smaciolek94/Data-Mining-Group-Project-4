3a25b0f22515109b94ba3c90107d78d5a17dde54
Stefan Maciolek <51088087+Smaciolek94@users.noreply.github.com>
2019-10-28 10:04:41 -0400

    Add files via upload

--- assignment 4.R
+++ assignment 4.R
@@ -12,10 +12,25 @@ n<-6
 #kmeans clustering:
 k <- kmeans(x,n)
 
-#calculating the gap statistic for each
+############################GAP STATISTIC
+#kmeans
 gapstatkmeans <- clusGap(x,FUN = kmeans,K.max = 20,B = 10) # last argument is the total number of clusters
+#PAM
 gapstatpam <- clusGap(x,FUN = pam,K.max = 20,B = 10)
-gapstathier <- clusGap(x,FUN = hclust,K.max = 20, B=10)
+#hierachical
+#a seperate function is needed for hierachical 
+hclust1 <- function(x,k){
+  list(cluster=cutree(hclust(dist(x),method="ward.D"),k))
+}
+      #as an aside, dist is the distance matrix of x, cutree cuts the cluster tree at k clusters
+        #similar to proc cluster and then proc tree in SAS
+gapstathier <- clusGap(x,FUN = hclust1,K.max = 20, B=10)
 
+#plotting
 plot(gapstatkmeans)
 plot(gapstatpam)
+plot(gapstathier)
+
+#silhouette statistic:
+silhouette(kmeans(x,n))
+silhouette(pam(x,4))
