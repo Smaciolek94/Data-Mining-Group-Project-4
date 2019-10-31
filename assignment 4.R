54eabb25784e1ecedf6f4e65f5a7bd627fb0b4a7
Stefan Maciolek <51088087+Smaciolek94@users.noreply.github.com>
2019-10-31 09:18:26 -0400

    Add files via upload
    I added more comments to try and explain what's going on.  I'm still totally stumped on the second derivative section

--- assignment 4.R
+++ assignment 4.R
@@ -8,19 +8,18 @@ for (i in 1:10){
   x[,i] <- rnorm(100)
 }
 
-#number of clusters here:
-n<-6
-
-#kmeans clustering:
-k <- kmeans(x,n)
-
 ############################GAP STATISTIC
+#the clusGap function calculates the gap statistic for each number of clusters specified by K.max =
+#no need to loop it, but it will take some time to run
+
 #kmeans
 gapstatkmeans <- clusGap(x,FUN = kmeans,K.max = 20,B = 10) # last argument is the total number of clusters
+
 #PAM
 gapstatpam <- clusGap(x,FUN = pam,K.max = 20,B = 10)
+
 #hierachical
-#a seperate function is needed for hierachical 
+#for hierarchical, the function needs to be defined first
 hclust1 <- function(x,k){
   list(cluster=cutree(hclust(dist(x),method="ward.D"),k))
 }
@@ -33,12 +32,44 @@ plot(gapstatkmeans)
 plot(gapstatpam)
 plot(gapstathier)
 
-#silhouette statistic:
-#kmeans needs its own function defined first
-kmfunc <- kmeans(x,n)
-silhouette(kmfunc$cluster, dist(x)) #this one works now too
-silhouette(pam(x,n)) #this one works
-silhouette(hclust1, dist(x))
+print(min(gapstatpam$Tab[,3])) #the third column is the gap stat
+print(min(gapstatkmeans$Tab[,3])) #the third column is the gap stat
+print(min(gapstathier$Tab[,3])) #the third column is the gap stat
+
+################silhouette statistic:
+#the silhoutte function finds the silhoutte statistic for each point for a given number of clusters and a given 
+#clustering method.  I had to define a function that ran it for each clustering method, and then averaged
+#all of the output silhoutte values to find the silhoutte statistic for that method for that number of clusters
+#it needs to be looped in order to find the minumum
+
+silh <- function(x,n){
+
+  kmfunc <- kmeans(x,n)
+  silkm <- silhouette(kmfunc$cluster, dist(x)) 
+  
+  pamfunc <- pam(x,n)
+  silpam <- silhouette(pamfunc$clustering, dist(x)) 
+  
+  outhier <- hclust1(x,n)
+  silhier <- silhouette(as.numeric(outhier$cluster), dist(x)) 
+    
+ #finding the averages and returning just them
+  avesilkm <- mean(silkm[,3])
+  avesilpam <- mean(silpam[,3])
+  avesilhier <- mean(silhier[,3])
+  out <- c(avesilkm,avesilpam,avesilhier)
+  return(out)
+}
+#running the function in a loop for each possible number of clusters
+
+p <- silh(x,6) # it works when run once, but not in a cluster 
+#im still getting an error with the number of dimensions when trying to loop, but the function works when run by itself
+out <- rep(0,60)
+dim(out) <- c(20,3)
+for (n in 1:20){
+  out[n,1:3] <- silh(x,n)
+}
 
 #second derivative:
 sdkmeans <- sapply(1:20, FUN = kmeans(x,7))
+sdpam <- sapply(1:20, FUN = pam(x,n))
